4a2cc775009470ef3f8e06606a0a5dafeab84770
Paolo Cavagnolo <paolo.cavagnolo@gmail.com>
2023-09-13 13:46:32 +0200

    Create frames.h

---
+++ demo_02-R4-game/frames.h
@@ -0,0 +1,71 @@
+const uint32_t s_opensource[] = {
+  0x3c042099,
+  0xa50a50a,
+  0x50420240,
+};
+
+const uint32_t s_is[] = {
+  0x10008,
+  0xf400801,
+  0x0,
+};
+
+const uint32_t s_love[] = {
+  0x63094888,
+  0x88084102,
+  0x20140080,
+};
+
+const uint32_t s_arduino[] = {
+  0x71c8a,
+  0x2a4a8a27,
+  0x1c000000,
+};
+
+const uint32_t s_lover[] = {
+  0x500a8088,
+  0x51422a0,
+  0x22014008,
+};
+
+const uint32_t s_keepcalm[] = {
+  0x842ca6aa,
+  0xa912802f,
+  0xfe802ffe,
+};
+
+const uint32_t s_and[] = {
+  0x20020,
+  0xf802002,
+  0x0,
+};
+
+const uint32_t s_eat[] = {
+  0x38044088,
+  0x9158804,
+  0x40380000,
+};
+
+const uint32_t s_code[] = {
+  0x24042,
+  0x8104202,
+  0x40000000,
+};
+
+const uint32_t s_peace[] = {
+  0x38054092,
+  0x920ba05,
+  0x40380000,
+};
+
+const uint32_t blank[] = {
+  0x0,
+  0x0,
+  0x0,
+};
+
+const uint32_t s_question[] = {
+  0xe011011,
+  0x200400,
+  0x40000040,
+};
